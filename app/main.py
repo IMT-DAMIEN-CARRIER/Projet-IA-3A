3964d67156e9a64a8a5f98c836a68b245e81e7ea
Clément <clement.savinaud@mines-ales.org>
2021-10-01 09:14:19 +0200

    Tests

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 ## IMPORTS ##
+from models.DecrypterModel import DecrypterModel
 import torch
 import matplotlib.pyplot as plt
+import numpy as np
 
 ## FROM ##
 from classes.LoadImages import *
+from utils import *
 
 ## Functions ##
 
@@ -25,5 +28,17 @@ def loadData(train_path: str, train_original_path: str):
     #
     #    break
 
+D_out = 10     # output dimension
 
 data_loader = loadData("dataset/1A/train_1A_tiny.npy","dataset/original/train_original_tiny.npy")
+# batch shape : (64, 3, 96, 96)
+
+train, test = iter(data_loader).next()
+print(train.shape)
+print(test.shape)
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+model = DecrypterModel(D_out)
+
+train_optim(model, data_loader, epochs=1, log_frequency=60, device=device)
--- app/utils.py
+++ app/utils.py
@@ -1,54 +1,53 @@
 import torch
 
 ## Utils functions ##
+def train_optim(model, data_loader, epochs, log_frequency, device, learning_rate=1e-4):
+    model.to(device) # we make sure the model is on the proper device
 
-def train_optim(model, trainloader, testloader, epochs, log_frequency, device, learning_rate=1e-4):
+    # Multiclass classification setting, we use cross-entropy
+    # note that this implementation requires the logits as input 
+    # logits: values prior softmax transformation 
+    loss_fn = torch.nn.CrossEntropyLoss(reduction='mean')
 
-  model.to(device) # we make sure the model is on the proper device
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    
+    for t in range(epochs):
+        model.train() # we specify that we are training the model
 
-  # Multiclass classification setting, we use cross-entropy
-  # note that this implementation requires the logits as input 
-  # logits: values prior softmax transformation 
-  loss_fn = torch.nn.nn.MSELoss()
+        trainloader, testloader = iter(data_loader).next()
 
-  optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-  
-  for t in range(epochs):
+        # At each epoch, the training set will be processed as a set of batches
+        for batch_id,  batch in enumerate(trainloader) : 
 
-      model.train() # we specify that we are training the model
+            images, labels  = batch
 
-      # At each epoch, the training set will be processed as a set of batches
-      for batch_id,  batch in enumerate(trainloader) : 
+            # we put the data on the same device
+            images , labels = images.to(device), labels.to(device)  
+            
+            y_pred = model(images) # forward pass output=logits
 
-        images, labels  = batch
+            loss = loss_fn(y_pred, labels)
 
-        # we put the data on the same device
-        images , labels = images.to(device), labels.to(device)  
+            if batch_id % log_frequency == 0:
+                print("epoch: {:03d}, batch: {:03d}, loss: {:.3f} ".format(t+1, batch_id+1, loss.item()))
+
+            optimizer.zero_grad() # clear the gradient before backward
+            loss.backward()       # update the gradient
+
+            optimizer.step() # update the model parameters using the gradient
+
+        # Model evaluation after each step computing the accuracy
+        model.eval()
+        total = 0
+        correct = 0
+        for batch_id, batch in enumerate(testloader):
+            images , labels = batch
+            images , labels = images.to(device), labels.to(device)
+            y_pred = model(images) # forward computes the logits
+            sf_y_pred = torch.nn.Softmax(dim=1)(y_pred) # softmax to obtain the probability distribution
+            _, predicted = torch.max(sf_y_pred , 1)     # decision rule, we select the max
+            
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
         
-        y_pred = model(images) # forward pass output=logits
-
-        loss = loss_fn(y_pred, labels)
-
-        if batch_id % log_frequency == 0:
-            print("epoch: {:03d}, batch: {:03d}, loss: {:.3f} ".format(t+1, batch_id+1, loss.item()))
-
-        optimizer.zero_grad() # clear the gradient before backward
-        loss.backward()       # update the gradient
-
-        optimizer.step() # update the model parameters using the gradient
-
-      # Model evaluation after each step computing the accuracy
-      model.eval()
-      total = 0
-      correct = 0
-      for batch_id, batch in enumerate(testloader):
-        images , labels = batch
-        images , labels = images.to(device), labels.to(device)
-        y_pred = model(images) # forward computes the logits
-        sf_y_pred = torch.nn.Softmax(dim=1)(y_pred) # softmax to obtain the probability distribution
-        _, predicted = torch.max(sf_y_pred , 1)     # decision rule, we select the max
-        
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-      
-      print("[validation] accuracy: {:.3f}%\n".format(100 * correct / total))
+        print("[validation] accuracy: {:.3f}%\n".format(100 * correct / total))
