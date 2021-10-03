297cd24f4ee7f0bb80b1349027bfef63ea88d0ce
Clément <clement.savinaud@mines-ales.org>
2021-10-03 13:45:33 +0200

    Modification du Decrypter

--- app/models/DecrypterModel.py
+++ app/models/DecrypterModel.py
@@ -8,10 +8,10 @@ class DecrypterModel(torch.nn.Module):
         self.model.classifier = torch.nn.Sequential(*[torch.nn.Linear(in_features=25088, out_features=4096, bias=True),
                                                     torch.nn.ReLU(),
                                                     torch.nn.Dropout(p=0.5),
-                                                    torch.nn.Linear(in_features=4096, out_features=2048, bias=True),
+                                                    torch.nn.Linear(in_features=4096, out_features=4096, bias=True),
                                                     torch.nn.ReLU(),
                                                     torch.nn.Dropout(p=0.5),
-                                                    torch.nn.Linear(in_features=2048, out_features=d_out, bias=True)])
+                                                    torch.nn.Linear(in_features=4096, out_features=d_out, bias=True)])
 
     def forward(self, x):
         return self.model(x)
