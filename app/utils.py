556f7b18118eaeaac3715e0f411b09b4d05efc1a
Clément <clement.savinaud@mines-ales.org>
2021-10-05 10:01:26 +0200

    Mise au propre du code + ajout métrique eval + ajout sauvegarde résultats

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,5 @@
 import torch
+import matplotlib.pyplot as plt
 
 ## Utils functions ##
 def train_optim(model, trainloader, testloader, epochs, log_frequency, device, learning_rate=1e-4):
@@ -7,7 +8,7 @@ def train_optim(model, trainloader, testloader, epochs, log_frequency, device, l
     # Multiclass classification setting, we use cross-entropy
     # note that this implementation requires the logits as input
     # logits: values prior softmax transformation
-    loss_fn = torch.nn.MSELoss()
+    loss_fn = torch.nn.MSELoss() # Fonction de loss pour de la régression
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
     for t in range(epochs):
@@ -29,23 +30,48 @@ def train_optim(model, trainloader, testloader, epochs, log_frequency, device, l
             optimizer.zero_grad()  # clear the gradient before backward
             loss.backward()  # update the gradient
             optimizer.step()  # update the model parameters using the gradient
+            #break
 
         # Model evaluation after each step computing the accuracy
+        print('Starting evaluation')
         model.eval()
         total = 0
         correct = 0
         for batch_id, batch in enumerate(testloader):
             images , labels = batch
             images , labels = images.to(device), labels.to(device)
-            y_pred = model(images) # forward computes the logits
-            sf_y_pred = torch.nn.Softmax(dim=1)(y_pred) # softmax to obtain the probability distribution
-            _, predicted = torch.max(sf_y_pred , 1)     # decision rule, we select the max
+            y_pred = model.forward(images) # forward computes the logits
+            y_pred = y_pred.reshape(images.shape[0], 3, 96, 96)
             
             total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-        
-        print("[validation] accuracy: {:.3f}%\n".format(100 * correct / total))
+            #correct += (y_pred == labels).sum().item()
+            y_pred = y_pred.detach()
 
+            # Metrique d'évaluation du model
+            print(eval_metric(labels, y_pred))
+
+            # Sauvegarde de l'image déterioré
+            plt.figure(1)
+            plt.imshow(images[0].permute(1, 2, 0))
+            plt.show()
+            plt.savefig('images/base')
+
+            # Sauvegarde de l'image attendu
+            plt.figure(1)
+            plt.imshow(labels[0].permute(1, 2, 0))
+            plt.show()
+            plt.savefig('images/attendu')
+
+            # Sauvegarde de notre prediction
+            plt.figure(1)
+            plt.imshow(y_pred[0].permute(1, 2, 0))
+            plt.show()
+            plt.savefig('images/prediction')
+        
+            #break
+
+        
+        #print("[validation] accuracy: {:.3f}%\n".format(100 * correct / total))
 
 def eval_metric(img, pred):
-    return torch.abs(img - pred).sum()
+    return torch.abs(img - pred).sum().item()
