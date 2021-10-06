7192772da97116fc4c183b35cca87d698c61886c
Clément <clement.savinaud@mines-ales.org>
2021-10-06 08:29:47 +0200

    Utilisation du model Unet

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 ## IMPORTS ##
 from models.DecrypterModel import DecrypterModel
+from models.UnetModel import UNet
 import torch
 #import matplotlib.pyplot as plt
 import numpy as np
@@ -13,8 +14,11 @@ from torch.utils.data.dataloader import DataLoader
 ## Functions ##
 
 def loadData(train_path: str, train_original_path: str):
-    batch_size = 64
+    batch_size = 8
     dataset = LoadImages(train_path, train_original_path)
+    print(len(dataset))
+    #test, _ = torch.utils.data.random_split(dataset, [3, len(dataset) - 3])
+    #return DataLoader(test, batch_size, shuffle=True)
     return DataLoader(dataset, batch_size, shuffle=True)
 
     # for batch in dataloader:
@@ -34,10 +38,14 @@ torch.manual_seed(1234)
 D_out = 3*96*96     # output dimension
 
 train_loader = loadData("dataset/1A/train_1A_tiny.npy", "dataset/original/train_original_tiny.npy")
+print(len(train_loader))
+
+
 test_loader = loadData("dataset/1A/test_1A.npy", "dataset/original/test_original.npy")
 # batch shape : (64, 3, 96, 96)
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-model = DecrypterModel(D_out)
+#model = DecrypterModel(D_out)
+model = UNet(3,D_out,False)
 
-train_optim(model, train_loader, test_loader, epochs=5, log_frequency=1, device=device)
+train_optim(model, train_loader, test_loader, epochs=1, log_frequency=1, device=device)
